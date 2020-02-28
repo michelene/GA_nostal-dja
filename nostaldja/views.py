3eea552e00249da3848f0531e7a20c4112f92ce4
Michelene Chon <michelenechon@gmail.com>
2020-02-28 14:06:55 -0800

    Add routes

--- nostaldja/views.py
+++ nostaldja/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Decade, Fad
+from .forms import DecadeForm, FadForm
 
-# Create your views here.
+
+def decade_list(request):
+    # get all the decades from the database
+    decades = Decade.objects.all()
+    # send the HTML template back to the client
+    return render(request, 'nostaldja/decade_list.html', {'decades': decades})
+
+
+def fad_list(request):
+    # get all the decades from the database
+    fads = Fad.objects.all()
+    # send the HTML template back to the client
+    return render(request, 'nostaldja/fad_list.html', {'fads': fads})
+
+
+def decade_detail(request, pk):
+    # look up decade by primary key
+    decade = Decade.objects.get(id=pk)
+    # send the detail page back to client
+    return render(request, 'nostaldja/decade_detail.html', {'decade': decade})
+
+
+def fad_detail(request, pk):
+    # look up fad by primary key
+    fad = Fad.objects.get(id=pk)
+    # send the detail page back to client
+    return render(request, 'nostaldja/fad_detail.html', {'fad': fad})
+
+
+def decade_create(request):
+    if request.method == 'POST':
+        form = DecadeForm(request.POST)
+        if form.is_valid():
+            decade = form.save()
+            return redirect('decade_detail', pk=decade.pk)
+    else:
+        form = DecadeForm()
+    return render(request, 'nostaldja/decade_form.html', {'form': form})
+
+
+def fad_create(request):
+    if request.method == 'POST':
+        form = FadForm(request.POST)
+        if form.is_valid():
+            fad = form.save()
+            return redirect('fad_detail', pk=fad.pk)
+    else:
+        form = FadForm()
+    return render(request, 'nostaldja/fad_form.html', {'form': form})
+
+
+def decade_edit(request, pk):
+    decade = Decade.objects.get(pk=pk)
+    if request.method == "POST":
+        form = DecadeForm(request.POST, instance=decade)
+        if form.is_valid():
+            decade = form.save()
+            return redirect('decade_detail', pk=decade.pk)
+    else:
+        form = DecadeForm(instance=decade)
+    return render(request, 'nostaldja/decade_form.html', {'form': form})
+
+
+def fad_edit(request, pk):
+    fad = Fad.objects.get(pk=pk)
+    if request.method == "POST":
+        form = FadForm(request.POST, instance=fad)
+        if form.is_valid():
+            fad = form.save()
+            return redirect('fad_detail', pk=fad.pk)
+    else:
+        form = FadForm(instance=fad)
+    return render(request, 'nostaldja/fad_form.html', {'form': form})
+
+
+def decade_delete(request, pk):
+    Decade.objects.get(id=pk).delete()
+    return redirect('decade_list')
+
+
+def fad_delete(request, pk):
+    Fad.objects.get(id=pk).delete()
+    return redirect('fad_list')
--- nostaldja_project/nostaldja_project/settings.sql
+++
@@ -1,4 +0,0 @@
--- settings.sql
-CREATE DATABASE nostaldja;
-CREATE USER nostaldjauser WITH PASSWORD 'nostaldjapassword';
-GRANT ALL PRIVILEGES ON DATABASE nostaldja TO nostaldjauser;
