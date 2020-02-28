ed00a8f23e35ffd0c4bee6363c6b55ebc4ba39c2
Michelene Chon <michelenechon@gmail.com>
2020-02-28 13:23:51 -0800

    Add models

--- nostaldja/models.py
+++ nostaldja/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Decade(models.Model):
+    start_year = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.start_year
+
+
+class Fad(models.Model):
+    decade = models.ForeignKey(
+        Decade, on_delete=models.CASCADE, related_name='fads')
+    name = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    decade = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
