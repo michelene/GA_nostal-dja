48b83154fd6994f59c0957b0f741e2d3419d4ab1
Michelene Chon <michelenechon@gmail.com>
2020-02-28 17:03:20 -0800

    Add minimal settings.sql

---
+++ nostaldja/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Decade, Fad
+
+
+class DecadeForm(forms.ModelForm):
+
+    class Meta:
+        model = Decade
+        fields = ('start_year',)
+
+
+class FadForm(forms.ModelForm):
+
+    class Meta:
+        model = Fad
+        fields = ('decade', 'name', 'image_url', 'description', 'decade',)
--- nostaldja/models.py
+++ nostaldja/models.py
@@ -12,9 +12,8 @@ class Fad(models.Model):
     decade = models.ForeignKey(
         Decade, on_delete=models.CASCADE, related_name='fads')
     name = models.CharField(max_length=100)
-    image_url = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000)
-    decade = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=1000)
 
     def __str__(self):
         return self.name
